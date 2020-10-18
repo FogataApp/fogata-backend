0ac84ccbd025c97a6a59d87b06673dad0af71cc8
santiago.arcilag <santiago.arcilag@gmail.com>
2020-10-17 21:39:42 -0500

    CORS

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const { config } = require('./config/index')
 const fogataAPI = require('./routes/places')
